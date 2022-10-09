61a28628cc353398ca39e0a7e4057c4d28d3c9f9
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2022-10-09 12:01:21 +0200

    Add Initial Application

---
+++ rqms-runable/src/main/java/com/raynigon/rqms/runable/RqmsApplication.java
@@ -0,0 +1,13 @@
+package com.raynigon.rqms.runable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RqmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RqmsApplication.class, args);
+	}
+
+}
