3af46c838905de78798784ff3b4428103ab5de6f
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2022-10-12 20:28:30 +0200

    Update Test Data

--- rqms-api-general/angular/src/app/offline/judgment-lists-overview/judgment-lists-overview.component.ts
+++ rqms-api-general/angular/src/app/offline/judgment-lists-overview/judgment-lists-overview.component.ts
@@ -9,22 +9,22 @@ import { JudgmentList } from '../model/jugment-list';
 export class JudgmentListsOverviewComponent implements OnInit {
 
   public lists: JudgmentList[] = [
-    { id: "1234567890", title: "Wasser", tags: ["drink"], resultCount: 12, testScore: 1.0 },
-    { id: "1234567891", title: "Milch", tags: ["breakfast", "drink"], resultCount: 12, testScore: 0.9 },
-    { id: "1234567892", title: "Saft", tags: ["drink", "fruit"], resultCount: 12, testScore: 0.75 },
-    { id: "1234567892", title: "Kokosnuss", tags: ["fruit", "nut"], resultCount: 12, testScore: 0.99999999999 },
-    { id: "1234567892", title: "Karotte", tags: ["vegetables", "tuber"], resultCount: 12, testScore: 0.99999999999 },
-    { id: "1234567892", title: "Karotte", tags: ["vegetables", "tuber"], resultCount: 12, testScore: 1.0 },
-    { id: "1234567892", title: "Karotte", tags: ["vegetables", "tuber"], resultCount: 12, testScore: 0.9 },
-    { id: "1234567892", title: "Karotte", tags: ["vegetables", "tuber"], resultCount: 12, testScore: 0.8 },
-    { id: "1234567892", title: "Karotte", tags: ["vegetables", "tuber"], resultCount: 12, testScore: 0.7 },
-    { id: "1234567892", title: "Karotte", tags: ["vegetables", "tuber"], resultCount: 12, testScore: 0.6 },
-    { id: "1234567892", title: "Karotte", tags: ["vegetables", "tuber"], resultCount: 12, testScore: 0.5 },
-    { id: "1234567892", title: "Karotte", tags: ["vegetables", "tuber"], resultCount: 12, testScore: 0.4 },
-    { id: "1234567892", title: "Karotte", tags: ["vegetables", "tuber"], resultCount: 12, testScore: 0.3 },
-    { id: "1234567892", title: "Karotte", tags: ["vegetables", "tuber"], resultCount: 12, testScore: 0.2 },
-    { id: "1234567892", title: "Karotte", tags: ["vegetables", "tuber"], resultCount: 12, testScore: 0.1 },
-    { id: "1234567892", title: "Karotte", tags: ["vegetables", "tuber"], resultCount: 12, testScore: 0.0 },
+    { id: "1234567890", title: "Wasser", tags: ["category:drink"], resultCount: 12, testScore: 1.0 },
+    { id: "1234567892", title: "Kokosnuss", tags: ["category:fruit", "subcategory:nut"], resultCount: 12, testScore: 0.99999999999 },
+    { id: "1234567892", title: "Karotte", tags: ["category:vegetables", "subcategory:tuber"], resultCount: 12, testScore: 0.99999999999 },
+    { id: "1234567892", title: "Karotte", tags: ["category:vegetables", "subcategory:tuber"], resultCount: 12, testScore: 1.0 },
+    { id: "1234567891", title: "Milch", tags: ["meal:breakfast", "category:drink"], resultCount: 12, testScore: 0.9 },
+    { id: "1234567892", title: "Karotte", tags: ["category:vegetables", "subcategory:tuber"], resultCount: 12, testScore: 0.9 },
+    { id: "1234567892", title: "Karotte", tags: ["category:vegetables", "subcategory:tuber"], resultCount: 12, testScore: 0.8 },
+    { id: "1234567892", title: "Saft", tags: ["category:drink", "category:fruit"], resultCount: 12, testScore: 0.75 },
+    { id: "1234567892", title: "Karotte", tags: ["category:vegetables", "subcategory:tuber"], resultCount: 12, testScore: 0.7 },
+    { id: "1234567892", title: "Karotte", tags: ["category:vegetables", "subcategory:tuber"], resultCount: 12, testScore: 0.6 },
+    { id: "1234567892", title: "Karotte", tags: ["category:vegetables", "subcategory:tuber"], resultCount: 12, testScore: 0.5 },
+    { id: "1234567892", title: "Karotte", tags: ["category:vegetables", "subcategory:tuber"], resultCount: 12, testScore: 0.4 },
+    { id: "1234567892", title: "Karotte", tags: ["category:vegetables", "subcategory:tuber"], resultCount: 12, testScore: 0.3 },
+    { id: "1234567892", title: "Karotte", tags: ["category:vegetables", "subcategory:tuber"], resultCount: 12, testScore: 0.2 },
+    { id: "1234567892", title: "Karotte", tags: ["category:vegetables", "subcategory:tuber"], resultCount: 12, testScore: 0.1 },
+    { id: "1234567892", title: "Karotte", tags: ["category:vegetables", "subcategory:tuber"], resultCount: 12, testScore: 0.0 },
   ]
 
   constructor() { }
