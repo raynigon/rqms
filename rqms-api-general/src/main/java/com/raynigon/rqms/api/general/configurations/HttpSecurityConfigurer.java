a8537e3ed81437da7636282c4de519e9e9a326c8
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2022-10-21 12:53:01 +0200

    Update Spring

--- rqms-api-general/src/main/java/com/raynigon/rqms/api/general/configurations/HttpSecurityConfigurer.java
+++ rqms-api-general/src/main/java/com/raynigon/rqms/api/general/configurations/HttpSecurityConfigurer.java
@@ -2,14 +2,14 @@ package com.raynigon.rqms.api.general.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 //@EnableConfigurationProperties(OidcProperties::class)
 class HttpSecurityConfigurer {
 
